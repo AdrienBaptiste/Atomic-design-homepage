d588d97e88882975a554dd365ff6914db4d87ccf
Adrien BAPTISTE <adrienbaptiste@live.fr>
2024-10-28 15:12:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/atoms/Button'
+import MainLayout from './components/templates/MainLayout'
+
+function App() {
+
+  return (
+    <>
+      <MainLayout />
+      <Button />
+    </>
+  )
+}
+
+export default App
